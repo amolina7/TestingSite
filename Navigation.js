04f2f07f594620f909d07983f23d68ea5943c58f
amolina7 <ale.molina44@gmail.com>
2017-09-01 04:05:11 -0700

    Navigation
    Added Navigation.js that will be used for the main tabs, such as Home, List, Contact, etc...

---
+++ Navigation.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export class Navigation extends Component{
+
+  render() {
+    return(
+      <div>
+        <p> Check it out! </p>
+      </div>
+    );
+  }
+}
